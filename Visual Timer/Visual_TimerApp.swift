de866060fad817dbe5703e4f5ef7132d432f5743
Dan Fakkeldy <2112591+dfakkeldy@users.noreply.github.com>
2026-05-17 12:54:12 -0300

    Initial Commit

---
+++ Visual Timer/Visual_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Visual_TimerApp.swift
+//  Visual Timer
+//
+//  Created by Dan Fakkeldy on 2026-05-17.
+//
+
+import SwiftUI
+
+@main
+struct Visual_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
